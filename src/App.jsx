e2619fb2a8e5bbcd3c5353a35ebe265dedc396ea
Tor-HodeKode <tor.kodehode@gmail.com>
2025-09-08 13:29:30 +0200

    More fixing
    Fixing x2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MainPage from "./pages/Mainpage"; // MainPage som viser prosjektene
-import Login from "./pages/Login"; // Login-siden
+import MainPage from "./Pages/Mainpage"; // MainPage som viser prosjektene
+import Login from "./Pages/Login"; // Login-siden
 import Crypto from "./Pages/Crypto"; // Crypto side (tar inn Crypto.jsx)
 import DrumKit from "./pages/Drumkit"; // Legger til import for DrumKit
 
 const App = () => {
   return (
     <Router basename="/TorFolio">
-      {" "}
       {/* 👈 Legger til basename her */}
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/projects/DrumKit" element={<DrumKit />} />
+        <Route path="/projects/drumkit" element={<DrumKit />} />
         <Route path="/projects/login" element={<Login />} />
         <Route path="/projects/crypto" element={<Crypto />} />
       </Routes>
