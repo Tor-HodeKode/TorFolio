d303176100b33b9576f59f11cb61ded1344d0fa8
Tor-HodeKode <tor.kodehode@gmail.com>
2025-09-08 14:37:28 +0200

    FixV3
    Third Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MainPage from "./Pages/Mainpage"; // MainPage som viser prosjektene
-import Login from "./Pages/Login"; // Login-siden
-import Crypto from "./Pages/Crypto"; // Crypto side (tar inn Crypto.jsx)
-import DrumKit from "./pages/Drumkit"; // Legger til import for DrumKit
+import MainPage from "./pages/mainpage"; // MainPage som viser prosjektene
+import Login from "./pages/login"; // Login-siden
+import Crypto from "./pages/crypto"; // Crypto side (tar inn Crypto.jsx)
+import DrumKit from "./pages/drumkit"; // Legger til import for DrumKit
 
 const App = () => {
   return (
     <Router basename="/TorFolio">
-      {/ /}
+      {/* ...existing code... */}
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/projects/drumkit" element={<DrumKit />} />
